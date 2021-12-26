7b36b893614353260009dd091b7705a203b1dba0
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-12-26 00:25:49 -0300

    personalized exeption 'Domain' create

---
+++ src/model/exeption/DomainExeption.java
@@ -0,0 +1,10 @@
+package model.exeption;
+
+public class DomainExeption extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExeption(String msg) {
+		super(msg);
+	}
+
+}
